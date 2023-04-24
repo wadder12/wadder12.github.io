317ced5f9ce338d970add2f05146958f08c06119
ANDREW <awade75009@gmail.com>
2023-04-24 14:50:14 -0500

    some js and css changes

--- scripts.js
+++ scripts.js
@@ -85,3 +85,79 @@ document.addEventListener('DOMContentLoaded', () => {
   
   // Call the function when the page loads
   updateBotStatus();
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scrolling for internal links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  
+    // Scroll-triggered animation for icons
+    const icons = document.querySelectorAll('nav ul li a img');
+    window.addEventListener('scroll', () => {
+      icons.forEach(icon => {
+        const iconTop = icon.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (iconTop < windowHeight * 0.8) {
+          icon.classList.add('icon-fade-in');
+        }
+      });
+    });
+  
+    // Dark mode toggle
+    const darkModeToggle = document.createElement('button');
+    darkModeToggle.textContent = 'Toggle Dark Mode';
+    darkModeToggle.style.position = 'fixed';
+    darkModeToggle.style.top = '20px';
+    darkModeToggle.style.right = '20px';
+    document.body.appendChild(darkModeToggle);
+  
+    darkModeToggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+    });
+  });
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    // Dynamic year in the footer
+    const currentYear = new Date().getFullYear();
+    const yearSpan = document.createElement('span');
+    yearSpan.textContent = currentYear;
+    document.querySelector('footer p').appendChild(yearSpan);
+  
+    // Mobile navigation menu
+    const mobileNavButton = document.createElement('button');
+    mobileNavButton.innerHTML = '&#9776;'; // Unicode character for "hamburger" menu icon
+    mobileNavButton.classList.add('mobile-nav-button');
+    document.body.appendChild(mobileNavButton);
+  
+    mobileNavButton.addEventListener('click', () => {
+      const nav = document.querySelector('nav');
+      nav.classList.toggle('mobile-nav');
+    });
+  
+    // Animated progress bar
+    const progressBar = document.createElement('div');
+    progressBar.style.position = 'fixed';
+    progressBar.style.top = '0';
+    progressBar.style.left = '0';
+    progressBar.style.height = '4px';
+    progressBar.style.backgroundColor = '#f5f5f5';
+    progressBar.style.width = '0%';
+    progressBar.style.zIndex = '1001';
+    document.body.appendChild(progressBar);
+  
+    window.addEventListener('scroll', () => {
+      const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scrollPosition = window.scrollY;
+      const scrollPercentage = (scrollPosition / windowHeight) * 100;
+      progressBar.style.width = scrollPercentage + '%';
+    });
+  });
+  
